df3032cb05ff807927e444613e0efdde7f2eeb6b
0xMyrddin <0xmyrddin@pm.me>
2024-11-17 17:23:53 +0100

    chains: add geist

--- src/buildList.js
+++ src/buildList.js
@@ -4,6 +4,7 @@ const arbitrumSepolia = require("./tokens/arbitrum-sepolia.json");
 const arbitrumOne = require("./tokens/arbitrum-one.json");
 const alephZero = require("./tokens/aleph-zero.json");
 const apechain = require("./tokens/apechain.json");
+const geist = require("./tokens/geist.json");
 const gravity = require("./tokens/gravity.json");
 const molten = require("./tokens/molten.json");
 const proofOfPlay = require("./tokens/proof-of-play.json");
@@ -16,7 +17,7 @@ const xai = require("./tokens/xai.json");
 const BASE_URL = process.env.BASE_URL || "https://token-list.camelot.exchange"
 
 module.exports = function buildList() {
-  const tokens = [...arbitrumSepolia, ...arbitrumOne, ...alephZero, ...apechain, ...gravity, ...molten, ...proofOfPlay, ...rari, ...reya, ...sanko, ...winr, ...xai]
+  const tokens = [...arbitrumSepolia, ...arbitrumOne, ...alephZero, ...apechain, ...geist, ...gravity, ...molten, ...proofOfPlay, ...rari, ...reya, ...sanko, ...winr, ...xai]
   const processedTokens = JSON.parse(JSON.stringify(tokens).replace(/BASE_URL/g, BASE_URL))
   const parsed = version.split(".");
 
